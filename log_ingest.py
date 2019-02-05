00d71814d9bd967fda8db6b899c1a465adc645b8
Chaz Schlarp <schlarpc@gmail.com>
2019-02-04 22:38:31 -0800

    working template with basic feature set

---
+++ log_ingest.py
@@ -0,0 +1,125 @@
+import collections
+import datetime
+import functools
+import gzip
+import itertools
+import json
+import uuid
+import sys
+
+import boto3
+
+LOG_BATCH_MAX_ITEMS = 10000
+LOG_BATCH_MAX_BYTES = 1_048_576
+LOG_EVENT_OVERHEAD = 26
+
+
+class LogEvent(collections.namedtuple("LogEntry", ["timestamp", "message"])):
+    @property
+    def encoded_size(self):
+        return len(self.message)
+
+    def as_dict(self):
+        return dict(
+            timestamp=int(self.timestamp.timestamp() * 1000),
+            message=self.message.decode("utf-8"),
+        )
+
+
+@functools.lru_cache()
+def get_client(service, region_name=None):
+    kwargs = {"region_name": region_name} if region_name else {}
+    return boto3.client(service, **kwargs)
+
+
+def get_s3_stream(key_basename, body):
+    def _create_stream():
+        for message in body.iter_lines():
+            timestamp = datetime.datetime.strptime(
+                message.split(b"[", 1)[1].split(b"]")[0].decode("utf-8"),
+                "%d/%b/%Y:%H:%M:%S %z",
+            )
+            yield LogEvent(timestamp=timestamp, message=message)
+
+    stream = _create_stream()
+    first_event = []
+    for event in stream:
+        first_event.append(event)
+        bucket_name = event.message.split(b" ")[1].decode("utf-8")
+        break
+    return bucket_name, key_basename, itertools.chain(first_event, stream)
+
+
+def get_cloudfront_stream(key_basename, body):
+    def _create_stream():
+        skipped_messages = []
+        for line in gzip.GzipFile(fileobj=body):
+            message = line.rstrip(b"\r\n")
+            if message.startswith(b"#"):
+                skipped_messages.append(message)
+                continue
+            timestamp = datetime.datetime.strptime(
+                b" ".join(message.split(b"\t")[:2]).decode("utf-8"), "%Y-%m-%d %H:%M:%S"
+            )
+            for skipped_message in skipped_messages:
+                yield LogEvent(timestamp=timestamp, message=skipped_message)
+            skipped_messages = []
+            yield LogEvent(timestamp=timestamp, message=message)
+
+    distribution_name, log_name = key_basename.rstrip(".gz").split(".", 1)
+    return distribution_name, log_name, _create_stream()
+
+
+def get_event_stream(record):
+    service, key_basename = record["s3"]["object"]["key"].split("/", 1)
+    try:
+        streamer = {"s3": get_s3_stream, "cloudfront": get_cloudfront_stream}[service]
+    except KeyError:
+        raise ValueError(f"Unknown log prefix {service!r}")
+    s3 = get_client("s3", region_name=record["awsRegion"])
+    response = s3.get_object(
+        Bucket=record["s3"]["bucket"]["name"], Key=record["s3"]["object"]["key"]
+    )
+    return (service,) + streamer(key_basename, response["Body"])
+
+
+def write_events_to_log_stream(logs, group_name, stream_name, events):
+    kwargs = {}
+    batch = []
+    batch_size = 0
+    for event in sorted(events):
+        next_batch_size = batch_size + event.encoded_size + LOG_EVENT_OVERHEAD
+        if next_batch_size >= LOG_BATCH_MAX_BYTES or len(batch) >= LOG_BATCH_MAX_ITEMS:
+            response = logs.put_log_events(
+                logGroupName=group_name,
+                logStreamName=stream_name,
+                logEvents=batch,
+                **kwargs,
+            )
+            kwargs["sequenceToken"] = response["nextSequenceToken"]
+            batch = []
+            batch_size = 0
+        batch.append(event.as_dict())
+        batch_size += event.encoded_size + LOG_EVENT_OVERHEAD
+    if batch:
+        response = logs.put_log_events(
+            logGroupName=group_name,
+            logStreamName=stream_name,
+            logEvents=batch,
+            **kwargs,
+        )
+
+
+def handler(event, context=None):
+    print(json.dumps(event))
+    logs = get_client("logs")
+    for record in event["Records"]:
+        service, resource_name, log_name, stream = get_event_stream(record)
+        group_name = f"/aws/{service}/{resource_name}"
+        stream_name = f"{log_name}/" + str(uuid.uuid4())
+        logs.create_log_stream(logGroupName=group_name, logStreamName=stream_name)
+        write_events_to_log_stream(logs, group_name, stream_name, stream)
+
+
+if __name__ == "__main__":
+    print(handler(json.loads(sys.argv[1])))
---
+++ statics3.py
@@ -0,0 +1,564 @@
+from troposphere import (
+    AccountId,
+    GetAtt,
+    Join,
+    Output,
+    Ref,
+    Template,
+    AWS_PARTITION,
+    Region,
+    AccountId,
+    NoValue,
+    Parameter,
+    Equals,
+    Not,
+    If,
+    Select,
+)
+from troposphere.s3 import (
+    AbortIncompleteMultipartUpload,
+    Bucket,
+    BucketEncryption,
+    BucketPolicy,
+    LambdaConfigurations,
+    LifecycleConfiguration,
+    LifecycleRule,
+    LifecycleRuleTransition,
+    LoggingConfiguration,
+    MetricsConfiguration,
+    NotificationConfiguration,
+    PublicAccessBlockConfiguration,
+    ServerSideEncryptionByDefault,
+    ServerSideEncryptionRule,
+)
+from troposphere.cloudfront import (
+    CloudFrontOriginAccessIdentity,
+    CloudFrontOriginAccessIdentityConfig,
+    DefaultCacheBehavior,
+    Distribution,
+    DistributionConfig,
+    ForwardedValues,
+    LambdaFunctionAssociation,
+    Logging,
+    Origin,
+    S3OriginConfig,
+    ViewerCertificate,
+)
+from troposphere.sqs import Queue
+from troposphere.logs import LogGroup
+from troposphere.awslambda import Code, DeadLetterConfig, Function, Permission, Version
+from troposphere.iam import PolicyProperty, PolicyType, Role
+
+from awacs import logs, s3, sqs, sts
+from awacs.aws import Allow, PolicyDocument, Principal, Statement
+
+import datetime
+import hashlib
+import inspect
+import json
+import textwrap
+
+import log_ingest
+import packmodule
+
+Partition = Ref(AWS_PARTITION)
+
+
+def add_condition(template, name, condition):
+    template.add_condition(name, condition)
+    return name
+
+
+def create_template():
+    template = Template(Description="Static website hosted with S3 and CloudFront")
+
+    acm_certificate_arn = template.add_parameter(
+        Parameter(
+            "AcmCertificateArn",
+            Description="Existing ACM certificate ARN to use for serving TLS",
+            Type="String",
+            AllowedPattern="(arn:[^:]+:acm:[^:]+:[^:]+:certificate/.+|)",
+            Default="",
+        )
+    )
+
+    dns_names = template.add_parameter(
+        Parameter(
+            "DomainNames",
+            Description="Comma-separated list of additional domain names to serve",
+            Type="CommaDelimitedList",
+            Default="",
+        )
+    )
+
+    tls_protocol_version = template.add_parameter(
+        Parameter(
+            "TlsProtocolVersion",
+            Description="CloudFront TLS security policy, see https://amzn.to/2DR91Xq for details",
+            Type="String",
+            Default="TLSv1.1_2016",
+        )
+    )
+
+    log_retention_days = template.add_parameter(
+        Parameter(
+            "LogRetentionDays",
+            Description="Days to keep CloudFront, S3, and Lambda logs",
+            Type="Number",
+            AllowedValues=[
+                1,
+                3,
+                5,
+                7,
+                14,
+                30,
+                60,
+                90,
+                120,
+                150,
+                180,
+                365,
+                400,
+                545,
+                731,
+                1827,
+                3653,
+            ],
+            Default=365,
+        )
+    )
+
+    default_ttl_seconds = template.add_parameter(
+        Parameter(
+            "DefaultTtlSeconds",
+            Description="Cache TTL when not set by S3 object headers",
+            Type="Number",
+            Default=int(datetime.timedelta(minutes=5).total_seconds()),
+        )
+    )
+
+    using_acm_certificate = add_condition(
+        template, "UsingAcmCertificate", Not(Equals(Ref(acm_certificate_arn), ""))
+    )
+
+    using_dns_names = add_condition(
+        template, "UsingDnsNames", Not(Equals(Select(0, Ref(dns_names)), ""))
+    )
+
+    log_ingester_dlq = template.add_resource(
+        Queue(
+            "LogIngesterDLQ",
+            MessageRetentionPeriod=int(datetime.timedelta(days=14).total_seconds()),
+            KmsMasterKeyId="alias/aws/sqs",
+        )
+    )
+
+    log_ingester_role = template.add_resource(
+        Role(
+            "LogIngesterRole",
+            AssumeRolePolicyDocument=PolicyDocument(
+                Version="2012-10-17",
+                Statement=[
+                    Statement(
+                        Effect="Allow",
+                        Principal=Principal("Service", "lambda.amazonaws.com"),
+                        Action=[sts.AssumeRole],
+                    )
+                ],
+            ),
+            Policies=[
+                PolicyProperty(
+                    PolicyName="DLQPolicy",
+                    PolicyDocument=PolicyDocument(
+                        Version="2012-10-17",
+                        Statement=[
+                            Statement(
+                                Effect=Allow,
+                                Action=[sqs.SendMessage],
+                                Resource=[GetAtt(log_ingester_dlq, "Arn")],
+                            )
+                        ],
+                    ),
+                )
+            ],
+        )
+    )
+
+    log_ingester = template.add_resource(
+        Function(
+            "LogIngester",
+            Runtime="python3.6",
+            Handler="index.{}".format(log_ingest.handler.__name__),
+            Code=Code(ZipFile=packmodule.pack(inspect.getsource(log_ingest))),
+            MemorySize=256,
+            Timeout=300,
+            Role=GetAtt(log_ingester_role, "Arn"),
+            DeadLetterConfig=DeadLetterConfig(
+                TargetArn=GetAtt(log_ingester_dlq, "Arn")
+            ),
+        )
+    )
+
+    log_ingester_permission = template.add_resource(
+        Permission(
+            "LogIngesterPermission",
+            FunctionName=GetAtt(log_ingester, "Arn"),
+            Action="lambda:InvokeFunction",
+            Principal="s3.amazonaws.com",
+            SourceAccount=AccountId,
+        )
+    )
+
+    log_bucket = template.add_resource(
+        Bucket(
+            "LogBucket",
+            AccessControl="LogDeliveryWrite",
+            LifecycleConfiguration=LifecycleConfiguration(
+                Rules=[
+                    LifecycleRule(ExpirationInDays=1, Status="Enabled"),
+                    LifecycleRule(
+                        AbortIncompleteMultipartUpload=AbortIncompleteMultipartUpload(
+                            DaysAfterInitiation=1
+                        ),
+                        Status="Enabled",
+                    ),
+                ]
+            ),
+            NotificationConfiguration=NotificationConfiguration(
+                LambdaConfigurations=[
+                    LambdaConfigurations(
+                        Event="s3:ObjectCreated:*", Function=GetAtt(log_ingester, "Arn")
+                    )
+                ]
+            ),
+            BucketEncryption=BucketEncryption(
+                ServerSideEncryptionConfiguration=[
+                    ServerSideEncryptionRule(
+                        ServerSideEncryptionByDefault=ServerSideEncryptionByDefault(
+                            # if we use KMS, we can't read the logs
+                            SSEAlgorithm="AES256"
+                        )
+                    )
+                ]
+            ),
+            MetricsConfigurations=[MetricsConfiguration(Id="default")],
+            DependsOn=[log_ingester_permission],
+            PublicAccessBlockConfiguration=PublicAccessBlockConfiguration(
+                BlockPublicAcls=True,
+                BlockPublicPolicy=True,
+                IgnorePublicAcls=True,
+                RestrictPublicBuckets=True,
+            ),
+        )
+    )
+
+    log_ingester_log_group = template.add_resource(
+        LogGroup(
+            "LogIngesterLogGroup",
+            LogGroupName=Join("", ["/aws/lambda/", Ref(log_ingester)]),
+            RetentionInDays=Ref(log_retention_days),
+        )
+    )
+
+    log_ingester_policy = template.add_resource(
+        PolicyType(
+            "LogIngesterPolicy",
+            Roles=[Ref(log_ingester_role)],
+            PolicyName="IngestLogPolicy",
+            PolicyDocument=PolicyDocument(
+                Version="2012-10-17",
+                Statement=[
+                    Statement(
+                        Effect=Allow,
+                        Action=[logs.CreateLogStream, logs.PutLogEvents],
+                        Resource=[
+                            Join(
+                                ":",
+                                [
+                                    "arn",
+                                    Partition,
+                                    "logs",
+                                    Region,
+                                    AccountId,
+                                    "log-group",
+                                    "/aws/cloudfront/*",
+                                ],
+                            ),
+                            Join(
+                                ":",
+                                [
+                                    "arn",
+                                    Partition,
+                                    "logs",
+                                    Region,
+                                    AccountId,
+                                    "log-group",
+                                    "/aws/s3/*",
+                                ],
+                            ),
+                            GetAtt(log_ingester_log_group, "Arn"),
+                        ],
+                    ),
+                    Statement(
+                        Effect=Allow,
+                        Action=[s3.GetObject],
+                        Resource=[Join("", [GetAtt(log_bucket, "Arn"), "/*"])],
+                    ),
+                ],
+            ),
+        )
+    )
+
+    bucket = template.add_resource(
+        Bucket(
+            "ContentBucket",
+            LifecycleConfiguration=LifecycleConfiguration(
+                Rules=[
+                    # not supported by CFN yet:
+                    # LifecycleRule(
+                    # Transitions=[
+                    # LifecycleRuleTransition(
+                    # StorageClass='INTELLIGENT_TIERING',
+                    # TransitionInDays=1,
+                    # ),
+                    # ],
+                    # Status="Enabled",
+                    # ),
+                    LifecycleRule(
+                        AbortIncompleteMultipartUpload=AbortIncompleteMultipartUpload(
+                            DaysAfterInitiation=7
+                        ),
+                        Status="Enabled",
+                    )
+                ]
+            ),
+            LoggingConfiguration=LoggingConfiguration(
+                DestinationBucketName=Ref(log_bucket), LogFilePrefix="s3/"
+            ),
+            BucketEncryption=BucketEncryption(
+                ServerSideEncryptionConfiguration=[
+                    ServerSideEncryptionRule(
+                        ServerSideEncryptionByDefault=ServerSideEncryptionByDefault(
+                            # Origin Access Identities can't use KMS
+                            SSEAlgorithm="AES256"
+                        )
+                    )
+                ]
+            ),
+            MetricsConfigurations=[MetricsConfiguration(Id="default")],
+            PublicAccessBlockConfiguration=PublicAccessBlockConfiguration(
+                BlockPublicAcls=True,
+                BlockPublicPolicy=True,
+                IgnorePublicAcls=True,
+                RestrictPublicBuckets=True,
+            ),
+        )
+    )
+
+    origin_access_identity = template.add_resource(
+        CloudFrontOriginAccessIdentity(
+            "CloudFrontIdentity",
+            CloudFrontOriginAccessIdentityConfig=CloudFrontOriginAccessIdentityConfig(
+                Comment=GetAtt(bucket, "Arn")
+            ),
+        )
+    )
+
+    bucket_policy = template.add_resource(
+        BucketPolicy(
+            "ContentBucketPolicy",
+            Bucket=Ref(bucket),
+            PolicyDocument=PolicyDocument(
+                Version="2012-10-17",
+                Statement=[
+                    Statement(
+                        Effect=Allow,
+                        Principal=Principal(
+                            "CanonicalUser",
+                            GetAtt(origin_access_identity, "S3CanonicalUserId"),
+                        ),
+                        Action=[s3.GetObject],
+                        Resource=[Join("", [GetAtt(bucket, "Arn"), "/*"])],
+                    )
+                ],
+            ),
+        )
+    )
+
+    log_bucket_policy = template.add_resource(
+        BucketPolicy(
+            "LogBucketPolicy",
+            Bucket=Ref(log_bucket),
+            PolicyDocument=PolicyDocument(
+                Version="2012-10-17",
+                Statement=[
+                    Statement(
+                        Sid="Allow CloudFront log delivery",
+                        Effect=Allow,
+                        Principal=Principal(
+                            "CanonicalUser",
+                            [
+                                # this translates to 162777425019 when saved...
+                                "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+                            ],
+                        ),
+                        Action=[s3.PutObject],
+                        Resource=[Join("", [GetAtt(log_bucket, "Arn"), "/*"])],
+                    )
+                ],
+            ),
+        )
+    )
+
+    edge_hook_role = template.add_resource(
+        Role(
+            "EdgeHookRole",
+            AssumeRolePolicyDocument=PolicyDocument(
+                Version="2012-10-17",
+                Statement=[
+                    Statement(
+                        Effect="Allow",
+                        Principal=Principal(
+                            "Service",
+                            ["lambda.amazonaws.com", "edgelambda.amazonaws.com"],
+                        ),
+                        Action=[sts.AssumeRole],
+                    )
+                ],
+            ),
+            ManagedPolicyArns=[
+                "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+            ],
+        )
+    )
+
+    edge_hook_function = template.add_resource(
+        Function(
+            "EdgeHookFunction",
+            Runtime="nodejs8.10",
+            Handler="index.handler",
+            Code=Code(
+                ZipFile=textwrap.dedent(
+                    """
+                    'use strict';
+                    exports.handler = (event, context, callback) => {
+                        var request = event.Records[0].cf.request;
+                        request.uri = request.uri.replace(/\/$/, '\/index.html');
+                        return callback(null, request);
+                    };
+                    """
+                )
+            ),
+            MemorySize=128,
+            Timeout=3,
+            Role=GetAtt(edge_hook_role, "Arn"),
+        )
+    )
+    edge_hook_function_hash = (
+        hashlib.sha256(
+            json.dumps(edge_hook_function.to_dict(), sort_keys=True).encode("utf-8")
+        )
+        .hexdigest()[:10]
+        .upper()
+    )
+
+    edge_hook_version = template.add_resource(
+        Version(
+            "EdgeHookVersion" + edge_hook_function_hash,
+            FunctionName=GetAtt(edge_hook_function, "Arn"),
+        )
+    )
+
+    distribution = template.add_resource(
+        Distribution(
+            "ContentDistribution",
+            DistributionConfig=DistributionConfig(
+                Enabled=True,
+                Aliases=If(using_dns_names, Ref(dns_names), NoValue),
+                Logging=Logging(
+                    Bucket=GetAtt(log_bucket, "DomainName"), Prefix="cloudfront/"
+                ),
+                DefaultRootObject="index.html",
+                Origins=[
+                    Origin(
+                        Id="default",
+                        DomainName=GetAtt(bucket, "DomainName"),
+                        S3OriginConfig=S3OriginConfig(
+                            OriginAccessIdentity=Join(
+                                "",
+                                [
+                                    "origin-access-identity/cloudfront/",
+                                    Ref(origin_access_identity),
+                                ],
+                            )
+                        ),
+                    )
+                ],
+                DefaultCacheBehavior=DefaultCacheBehavior(
+                    TargetOriginId="default",
+                    Compress=True,
+                    ForwardedValues=ForwardedValues(QueryString=False),
+                    ViewerProtocolPolicy="redirect-to-https",
+                    DefaultTTL=Ref(default_ttl_seconds),
+                    LambdaFunctionAssociations=[
+                        LambdaFunctionAssociation(
+                            EventType="origin-request",
+                            LambdaFunctionARN=Ref(edge_hook_version),
+                        )
+                    ],
+                ),
+                HttpVersion="http2",
+                IPV6Enabled=True,
+                ViewerCertificate=If(
+                    using_acm_certificate,
+                    ViewerCertificate(
+                        AcmCertificateArn=Ref(acm_certificate_arn),
+                        SslSupportMethod="sni-only",
+                        MinimumProtocolVersion=Ref(tls_protocol_version),
+                    ),
+                    ViewerCertificate(
+                        CloudFrontDefaultCertificate=True,
+                        MinimumProtocolVersion=Ref(tls_protocol_version),
+                    ),
+                ),
+            ),
+            DependsOn=[log_ingester_policy],
+        )
+    )
+
+    distribution_log_group = template.add_resource(
+        LogGroup(
+            "DistributionLogGroup",
+            LogGroupName=Join("", ["/aws/cloudfront/", Ref(distribution)]),
+            RetentionInDays=Ref(log_retention_days),
+        )
+    )
+
+    bucket_log_group = template.add_resource(
+        LogGroup(
+            "BucketLogGroup",
+            LogGroupName=Join("", ["/aws/s3/", Ref(bucket)]),
+            RetentionInDays=Ref(log_retention_days),
+        )
+    )
+
+    template.add_output(Output("DistributionId", Value=Ref(distribution)))
+
+    template.add_output(
+        Output("DistributionDomain", Value=GetAtt(distribution, "DomainName"))
+    )
+
+    template.add_output(
+        Output(
+            "DistributionUrl",
+            Value=Join("", ["https://", GetAtt(distribution, "DomainName"), "/"]),
+        )
+    )
+
+    template.add_output(Output("ContentBucketArn", Value=GetAtt(bucket, "Arn")))
+
+    return template
+
+
+if __name__ == "__main__":
+    print(create_template().to_json())
