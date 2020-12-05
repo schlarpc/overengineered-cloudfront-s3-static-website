be965a7098ff96f1f6fc29bb39b9cd7e794383d3
Chaz Schlarp <schlarpc@gmail.com>
2020-12-04 22:45:51 -0800

    Enable minification of template by default

--- overengineered_cloudfront_s3_static_website/__init__.py
+++ overengineered_cloudfront_s3_static_website/__init__.py
@@ -4,13 +4,13 @@ from .template import create_template
 
 def get_args(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--minify", action="store_true", default=False)
+    parser.add_argument("--no-minify", action="store_true", default=False)
     return parser.parse_args(argv)
 
 
 def main(argv=None):
     args = get_args(argv)
     json_kwargs = {"sort_keys": True}
-    if args.minify:
+    if not args.no_minify:
         json_kwargs.update({"indent": None, "separators": (",", ":")})
     print(create_template().to_json(**json_kwargs))
