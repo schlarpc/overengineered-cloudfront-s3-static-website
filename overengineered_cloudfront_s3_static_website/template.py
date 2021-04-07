60eab2ec5a21173d90a734c17d35af206f494f6c
Chaz Schlarp <schlarpc@gmail.com>
2021-04-06 19:18:36 -0700

    Add ap-northeast-3, an older existing region that was recently made broadly available

--- overengineered_cloudfront_s3_static_website/template.py
+++ overengineered_cloudfront_s3_static_website/template.py
@@ -176,6 +176,7 @@ def create_template():
                 "DefaultRegions": [
                     "ap-northeast-1",
                     "ap-northeast-2",
+                    "ap-northeast-3",
                     "ap-south-1",
                     "ap-southeast-1",
                     "ap-southeast-2",
